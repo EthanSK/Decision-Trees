badbb5f202828731202f23eb15b20d1de739eba6
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2020-01-25 02:49:52 +0000

    visualize saves to file

--- src/classification/visualize_tree.py
+++ src/classification/visualize_tree.py
@@ -1,16 +1,21 @@
 from .tree import BinTree
 from ..util.data_read import data_read
+from pathlib import Path
 
 # todo - write to file functionality
 
 
-def visualize_tree(tree: BinTree, max_deth: int = 10, save_file: str = None):
-    print(tree)
-    if save_file is not None:
-        pass
+def visualize_tree(tree: BinTree, max_deth: int = 10, save_filename: str = None):
+    tree_str = str(tree)
+    print(tree_str)
+    if save_filename is not None:
+        Path("out").mkdir(parents=True, exist_ok=True)
+        f = open("out/" + save_filename, "w+")
+        f.write(tree_str)
+        f.close()
 
 
 if __name__ == "__main__":
     dataset = data_read("data/toy.txt")
     tree = BinTree(dataset)
-    visualize_tree(tree)
+    visualize_tree(tree, save_filename="tree.txt")
