77f85ed53120a8f3549c3e2e5bcfea9e89724136
Ethan Sarif-Kattan <ethaneus99@gmail.com>
2020-01-26 02:38:43 +0000

    me and naim were being naughty

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def test_DecisionTreeClassifier():
     # train
     start = time.time()
     cl = DecisionTreeClassifier(should_load_file=False)
-    dataset = data_read("data/train_sub.txt")
+    dataset = data_read("data/toy.txt")
     x, y = dataset.shim_to_arrays()
     cl.train(x, y)
     cl.tree.save_tree()
@@ -27,7 +27,7 @@ def test_DecisionTreeClassifier():
     # evaluate
     ev = Evaluator()
     matrix = ev.confusion_matrix(preds, y_val)
-    print("\naccuracy: ", ev.accuracy(matrix))
+    print("\naccuracy: ", str.format('{0:.15f}', ev.accuracy(matrix)))
     print("\n precision: ", ev.precision(matrix))
     print("\n recall: ", ev.recall(matrix))
     print("\n f1_score: ", ev.f1_score(matrix))
@@ -40,7 +40,7 @@ def test_tree_load_file():
 
 def run_manual_test():
     start = time.time()
-    dataset = data_read("data/simple1.txt")
+    dataset = data_read("data/simple2.txt")
     tree = BinTree(dataset)
     visualize_tree(tree, max_depth=50)
     duration = time.time() - start
@@ -54,7 +54,7 @@ def old_test():
     class_labels = ["B", "A"]
     obj = Evaluator()
     matrix = obj.confusion_matrix(prediction, annotation, class_labels)
-    print(obj.accuracy(matrix))
+    print(str.format('{0:.15f}', obj.accuracy(matrix)))
     print(obj.precision(matrix))
     print(obj.recall(matrix))
     print(obj.f1_score(matrix))
